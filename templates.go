832534338bf55ba20fe855028a52d49fcefa3230
Yurii Zinets <yurii.zinets@icloud.com>
2021-03-12 14:19:09 +0100

    funcmap

--- templates.go
+++ templates.go
@@ -6,15 +6,27 @@ import (
 
 // TFMApply is a function to apply all function subsets to the provided funcmap
 func TFMApply(t *template.FuncMap) {
-
+	TFMApplyOperations(t)
+	TFMApplyDates(t)
+	TFMApplyOther(t)
 }
 
 // TFMApplyOperations is a function to apply operations subset to the provided funcmap
 func TFMApplyOperations(t *template.FuncMap) {
+	(*t)["sum"] = Sum
+	(*t)["sub"] = Sub
+	(*t)["mult"] = Mult
+	(*t)["div"] = Div
+}
 
+func TFMApplyDates(t *template.FuncMap) {
+	(*t)["currentyear"] = CurrentYear
+	(*t)["currentmonth"] = CurrentMonth
+	(*t)["currentday"] = CurrentDay
+	(*t)["currentdate"] = CurrentDateStr
 }
 
 // TFMApplyOther is a function to apply minor functions subset to the provided funcmap
 func TFMApplyOther(t *template.FuncMap) {
-
+	(*t)["range"] = Range
 }
