a8ad4f7164e34dc31727e905f5e0c3d9c8aa52b5
Yurii Zinets <yurii.zinets@icloud.com>
2021-03-16 08:59:22 +0100

    make range function inclusive

--- other.go
+++ other.go
@@ -3,7 +3,7 @@ package common
 // Range is a templace-specific helper to create range of numbers
 func Range(from, count int) []int {
 	rng := []int{}
-	for i := from; i < count; i++ {
+	for i := from; i <= count; i++ {
 		rng = append(rng, i)
 	}
 	return rng
--- other_test.go
+++ other_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestRange(t *testing.T) {
 	testrange := Range(1, 5)
-	expected := []int{1, 2, 3, 4}
+	expected := []int{1, 2, 3, 4, 5}
 	for i := 0; i < len(testrange); i++ {
 		if testrange[i] != expected[i] {
 			t.Error("Created test range is incorrect")
